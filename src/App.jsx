7c7ff6d783c1a61baebd20627d568537e8be13c8
unknown <olajamoyhinka@gmail.com>
2024-02-29 16:10:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import { WeatherApp } from './Components/Weather App/WeatherApp';
+
+function App() {
+    return (
+        <div className="App">
+            <WeatherApp/>
+        </div>
+    )
+  
+}
+
+export default App;
